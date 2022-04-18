e9e3b477a72b59a163ab855135f1583cf75f94b0
cespedesjuan <cespedesjuanpablo01@gmail.com>
2022-04-18 12:15:15 -0300

    entrega nr 7

---
+++ src/components/Cart.jsx
@@ -0,0 +1,71 @@
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { CartContext } from "../contexts/CartContext";
+import { WrapperCart, TitleCart, ContentCart, Product, ProductDetail, ImageCart, Details, PriceDetail, ProductAmountContainer, ProductAmount, ProductPrice } from "./styledComponents";
+import styled from "styled-components";
+
+const Top = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px;
+`;
+
+const TopButton = styled.button`
+  padding: 10px;
+  font-weight: 600;
+  cursor: pointer;
+  border: ${(props) => props.type === "filled" && "none"};
+  background-color: ${(props) =>
+        props.type === "filled" ? "black" : "transparent"};
+  color: ${(props) => props.type === "filled" && "white"};
+`;
+
+const TopText = styled.span`
+  margin: 0px 10px;
+`;
+
+const Cart = () => {
+    const test = useContext(CartContext);
+
+    return (
+        <WrapperCart>
+            <TitleCart>Carrito</TitleCart>
+            <Top>
+                <Link to='/'><TopButton>Seguir Comprando</TopButton></Link>
+                {
+                    (test.cartList.length > 0)
+                        ? <TopButton type="filled" onClick={test.borrarCarrito}>Borrar Carrito</TopButton>
+                        : <TopText>El carrito está vacío.</TopText>
+                }
+            </Top>
+            <ContentCart>
+                {
+                    test.cartList.length > 0 ?
+                        test.cartList.map(item =>
+                            <Product key={item.idItem}>
+                                <ProductDetail>
+                                    <ImageCart src={item.imgItem} />
+                                    <Details>
+                                        <span>
+                                            <b>Product: </b> {item.nameItem}
+                                        </span>
+                                        <TopButton type="filled" onClick={() => test.borrarItem(item.idItem)}>Borrar Item</TopButton>
+                                    </Details>
+                                </ProductDetail>
+                                <PriceDetail>
+                                    <ProductAmountContainer>
+                                        <ProductAmount>{item.qtyItem} item(s)</ProductAmount>
+                                    </ProductAmountContainer>
+                                    <ProductPrice>$ {item.costItem} c/u</ProductPrice>
+                                </PriceDetail>
+                            </Product>
+                        )
+                        : <TitleCart></TitleCart>
+                }
+            </ContentCart>
+        </WrapperCart>
+    );
+}
+
+export default Cart;
--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,6 +1,9 @@
+import { Button } from "@mui/material";
+import { useState, useContext } from "react";
 import ItemCount from "./ItemCount";
+import {Link} from 'react-router-dom';
 import { DetailContainer, WrapperDetail, ImgContainer, ImageDetail, InfoContainer, Title, Desc, Price } from "./styledComponents";
-
+import { CartContext } from '../contexts/CartContext'
 
 const ItemDetail = ({item}) => {
     const restar = () => {
